a6e30b039d1a52d15b82b90f7e7c9d5fbdb44cbd
wang <374951963@qq.com>
2016-09-19 22:02:39 +0800

    完成企业网站学习

---
+++ js/setHomeSetFav.js
@@ -0,0 +1,39 @@
+function AddFavorite(sURL, sTitle) {
+
+    sURL = encodeURI(sURL);
+    try {
+        window.external.addFavorite(sURL, sTitle);
+    }
+    catch (e) {
+
+        try {
+
+            window.sidebar.addPanel(sTitle, sURL, "");
+
+        }
+        catch (e) {
+
+            alert("�����ղ�ʧ�ܣ���ʹ��Ctrl+D�������,���ֶ�����������������.");
+
+        }
+
+    }
+
+}
+
+function SetHome(url) {
+
+    if (document.all) {
+
+        document.body.style.behavior = 'url(#default#homepage)';
+
+        document.body.setHomePage(url);
+
+    }
+    else {
+
+        alert("����,�����������֧���Զ�����ҳ��Ϊ��ҳ����,�����ֶ�������������ø�ҳ��Ϊ��ҳ!");
+
+    }
+
+}
